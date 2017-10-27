cf434756384601641a8f0076e35d1b60557f3aa1
Ethan Campbell <ethancc@uw.edu>
2017-10-27 00:36:47 -0700

    Initial commit

---
+++ moss.py
@@ -0,0 +1,70 @@
+"""
+
+This is a script to run iPython Notebook programming assignments through the MOSS plagiarism detection algorithm.
+Ethan C. Campbell, for OCEAN 215 (Autumn 2017), University of Washington
+
+#############
+
+One-time setup instructions for macOS:
+
+(1) obtain MOSS user ID by sending email to moss@moss.stanford.edu including only the following two lines of text:
+            registeruser
+            mail <<email@domain.suffix>>
+    where <<...>> is your email address
+
+(2a) in the automatic response email, look for your user ID (the line starts with '$userid=')
+(2b) set the variable 'moss_user_id' to this number
+
+(3) install Python client for MOSS using the following Terminal command (hopefully you have pip installed already):
+            pip install mosspy
+
+#############
+
+Instructions for checking a batch of homework assignments:
+
+(1) create an empty directory on your machine and save its path as the variable 'root_dir' below
+    this is where both the iPython Notebooks will be stored, and the MOSS results will be saved
+
+(2a) copy the original, blank assignment (an iPython Notebook) to this directory
+(2b) save its filename as the variable 'original_assignment' below
+
+(3a) copy all students' assignments (iPython Notebooks) to this directory
+(3b) check that all assignments have a common string within their filenames, such as 'A1.215'
+     if they don't, modify filenames to remedy this
+(3c) save that common string as the variable 'submission_string' below
+
+(4) run this script!
+
+(5) similarity report (an HTML file) is linked in output; the report is also saved locally to the specified directory
+
+"""
+
+import mosspy
+import os
+
+# run parameters
+moss_user_id = 181443923
+root_dir = '/Users/Ethan/Desktop/moss_submission/'
+original_assignment = 'ocean215.assignment1.final.ipynb'
+submission_string = 'A1.215'
+
+# convert all Notebooks to Python scripts
+os.system('jupyter nbconvert --to script {0}*.ipynb'.format(root_dir))
+
+# create new MOSS instance
+m = mosspy.Moss(moss_user_id, 'python')
+
+# set base assignment
+m.addBaseFile(root_dir + original_assignment)
+
+# submit files
+# m.addFile(root_dir + 'single_assignment.py')
+m.addFilesByWildcard(root_dir + '*{0}*.py'.format(submission_string))
+url = m.send()
+
+# save report file
+print('MOSS report URL: ' + url)
+m.saveWebPage(url, root_dir + 'report.html')
+
+# download whole report locally including code diff links
+mosspy.download_report(url, root_dir + 'report/', connections=8)
